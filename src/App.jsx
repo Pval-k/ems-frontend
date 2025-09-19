0bd0205569010fcc0a8f14955fed88ae6495b0e4
Pval-k <pravalhikak@gmail.com>
2025-09-18 19:21:02 -0700

    Added Header and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import FooterComponent from "./components/FooterComponent";
+import HeaderComponent from "./components/HeaderComponent";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
+import './App.css';
 
 function App() {
  
   return (
     <>
+      <HeaderComponent />
       <ListEmployeeComponent />
+      <FooterComponent />
     </>
   )
 }
