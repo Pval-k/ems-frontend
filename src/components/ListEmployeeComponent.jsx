7a77821c9ad98f49b090f2211e0e5bf4fa3d2b2f
Pval-k <pravalhikak@gmail.com>
2025-10-20 09:04:00 -0700

    Added Update Button, Title, and Route

--- src/components/ListEmployeeComponent.jsx
+++ src/components/ListEmployeeComponent.jsx
@@ -24,6 +24,10 @@ const ListEmployeeComponent = () => {
         navigator('/add-employee')
     }
 
+    function updateEmployee(id){
+        navigator(`/edit-employee/${id}`)
+    }
+
     // const employees = [
     //     {
     //     "id": 1,
@@ -56,6 +60,7 @@ const ListEmployeeComponent = () => {
                 <th>Employee First Name</th>
                 <th>Employee Last Name</th>
                 <th>Employee Email</th>
+                <th>Actions</th>
             </tr>
         </thead>
         <tbody>
@@ -66,7 +71,9 @@ const ListEmployeeComponent = () => {
                     <td>{employee.firstName}</td>
                     <td>{employee.lastName}</td>
                     <td>{employee.email}</td>
-
+                    <td>
+                        <button className='btn btn-info' onClick={() => updateEmployee(employee.id)}>Update</button>
+                    </td>
                 </tr>)
             }
             <tr>
